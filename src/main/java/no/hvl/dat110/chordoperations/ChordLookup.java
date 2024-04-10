af7b6385d84d45f452223a3724887931adac18a7
h599002 <h599002@stud.hvl.no>
2024-04-10 09:12:54 +0200

    forsøk oppg 3

--- src/main/java/no/hvl/dat110/chordoperations/ChordLookup.java
+++ src/main/java/no/hvl/dat110/chordoperations/ChordLookup.java
@@ -60,16 +60,25 @@ public class ChordLookup {
 	private NodeInterface findHighestPredecessor(BigInteger ID) throws RemoteException {
 		
 		// collect the entries in the finger table for this node
-		
+		List<NodeInterface> fingerTable = node.getFingerTable();
 		// starting from the last entry, iterate over the finger table
-		
-		// for each finger, obtain a stub from the registry
+		for(int i = fingerTable.size()-1; i>=0;i--){
+			// for each finger, obtain a stub from the registry
+			NodeInterface finger = fingerTable.get(i);
+			BigInteger fingerId = finger.getNodeID();
+			boolean isInInterval = Util.checkInterval(fingerId,node.getNodeID().add(BigInteger.ONE),ID.subtract(BigInteger.ONE));
+			if(isInInterval){
+				return finger;
+			}
+		}
+
 		
 		// check that finger is a member of the set {nodeID+1,...,ID-1} i.e. (nodeID+1 <= finger <= key-1) using the ComputeLogic
 		
 		// if logic returns true, then return the finger (means finger is the closest to key)
-		
-		return (NodeInterface) node;			
+
+		// In the case that no finger is the closest predecessor, we need to return the current node
+		return (NodeInterface) node;
 	}
 	
 	public void copyKeysFromSuccessor(NodeInterface succ) {
